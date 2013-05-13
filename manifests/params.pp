de8329ee749bef14d0ea892b4d954b043cdf8017
Spencer Herzberg <spencer.herzberg@gmail.com>
2013-05-13 09:07:02 -0500

    updating to logstash 1.1.12 as it seems to have a more stable web; version bump

--- manifests/params.pp
+++ manifests/params.pp
@@ -7,7 +7,7 @@ class logstash::params {
   $etc_dir = '/etc/logstash'
   $log_dir = '/var/log/logstash'
   $run_dir = '/var/run/logstash'
-  $version = '1.1.9'
+  $version = '1.1.12'
   $jar_file = "logstash-${version}-monolithic.jar"
   $source   = "http://semicomplete.com/files/logstash/${jar_file}"
   $bin_file = "${bin_dir}/${jar_file}"
